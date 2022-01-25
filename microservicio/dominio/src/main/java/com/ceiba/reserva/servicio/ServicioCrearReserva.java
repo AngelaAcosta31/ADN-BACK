dae9f66fe9628392e0e985f71b3efb44c1100948
Angela Maria Acosta Ramirez - Ceiba Software <angela.acosta@ceiba.com.co>
2022-01-25 09:58:53 -0500

    cambios para codigo limpio

--- microservicio/dominio/src/main/java/com/ceiba/reserva/servicio/ServicioCrearReserva.java
+++ microservicio/dominio/src/main/java/com/ceiba/reserva/servicio/ServicioCrearReserva.java
@@ -10,8 +10,6 @@ import com.ceiba.reserva.puerto.repositorio.RepositorioReserva;
 public class ServicioCrearReserva {
 
     private static final String LA_RESERVA_YA_EXISTE_EN_EL_SISTEMA = "La reserva ya existe en el sistema";
-    private static final String LA_HABITACION_NO_EXISTE_EN_EL_SISTEMA = "La habitacion no existe en el sistema";
-    private static final String LA_HABITACION_YA_ESTA_OCUPADA = "La habitacion ya se encuentra ocupada";
 
     private final RepositorioReserva repositorioReserva;
     private final DaoHabitacion daoHabitacion;
@@ -26,13 +24,7 @@ public class ServicioCrearReserva {
     }
 
     public Long ejecutar(Reserva reserva){
-
         validarExistenciaPrevia(reserva);
-        //validarExistenciaPreviaHabitacion(reserva.getIdHabitacion());
-        //validarExistenciaPreviaIdHabitacion(reserva.getIdHabitacion());
-
-        //reserva.setValor(precioHabitacion(reserva.getIdHabitacion()));
-
         return this.repositorioReserva.crear(reserva);
     }
 
@@ -43,23 +35,6 @@ public class ServicioCrearReserva {
         }
     }
 
-    private void validarExistenciaPreviaHabitacion(Long id){
-        boolean existe = this.repositorioHabitacion.existePorId(id);
-        if(!existe){
-            throw new ExcepcionSinDatos(LA_HABITACION_NO_EXISTE_EN_EL_SISTEMA);
-        }
-    }
 
-    private void validarExistenciaPreviaIdHabitacion(Long id){
-        boolean existe = this.repositorioReserva.existePorIdHabitacion(id);
-        if(existe){
-            throw new ExcepcionDuplicidad(LA_HABITACION_YA_ESTA_OCUPADA);
-        }
-    }
-
-
-    public Double precioHabitacion(Long idHabitacion){
-        return this.daoHabitacion.precioHabitacion(idHabitacion);
-    }
 
 }
